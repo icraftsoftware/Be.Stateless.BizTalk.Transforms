7df11ceab7734f9002862ada7ed93655daa98b3f
François Chabot <fchabot@skynet.be>
2020-07-17 17:39:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Be.Stateless.BizTalk.Transform.ExtensionObjects
+
+[![Build Status](https://dev.azure.com/icraftsoftware/be.stateless/_apis/build/status/Be.Stateless.BizTalk.Transform.ExtensionObjects%20Manual%20Release?branchName=master)](https://dev.azure.com/icraftsoftware/be.stateless/_build/latest?definitionId=666&branchName=master)
+[![GitHub Release](https://img.shields.io/github/v/release/icraftsoftware/Be.Stateless.BizTalk.Transform.ExtensionObjects)](https://github.com/icraftsoftware/Be.Stateless.BizTalk.Transform.ExtensionObjects/releases/latest)
+[![NuGet Version](https://img.shields.io/nuget/v/Be.Stateless.BizTalk.Transform.ExtensionObjects.svg?style=flat)](https://www.nuget.org/packages/Be.Stateless.BizTalk.Transform.ExtensionObjects/)
+
+BizTalk Factory's XSLT extension objects for general purpose BizTalk Server development.
---
+++ src/Be.Stateless.BizTalk.Transform.ExtensionObjects/Transform/ExtensionObjects/RegexFunctions.cs
@@ -0,0 +1,95 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2020 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using System.Xml.Xsl;
+
+namespace Be.Stateless.BizTalk.Transform.ExtensionObjects
+{
+	/// <summary>
+	/// XSLT extension object offering support for regular expressions.
+	/// </summary>
+	/// <seealso cref="XsltArgumentList.AddExtensionObject"/>
+	[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "XSLT Extension Object.")]
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "XSLT Extension Object.")]
+	public class RegexFunctions
+	{
+		/// <summary>
+		/// Indicates whether the specified regular expression finds a match in the specified input string.
+		/// </summary>
+		/// <param name="input">
+		/// The string to search for a match.
+		/// </param>
+		/// <param name="pattern">
+		/// The regular expression pattern to match.
+		/// </param>
+		/// <returns>
+		/// <c>true</c> if the regular expression finds a match; <c>false</c> otherwise.
+		/// </returns>
+		/// <seealso cref="Regex.IsMatch(string,string)"/>
+		[SuppressMessage("Performance", "CA1822:Mark members as static")]
+		public bool IsMatch(string input, string pattern)
+		{
+			return Regex.IsMatch(input, pattern);
+		}
+
+		/// <summary>
+		/// Indicates whether the specified regular expression finds a match in the <b>whole</b> specified input string.
+		/// </summary>
+		/// <param name="input">
+		/// The string to search for a match.
+		/// </param>
+		/// <param name="pattern">
+		/// The regular expression pattern to match.
+		/// </param>
+		/// <returns>
+		/// <c>true</c> if the regular expression finds a match; <c>false</c> otherwise.
+		/// </returns>
+		/// <seealso cref="Regex.IsMatch(string,string)"/>
+		[SuppressMessage("Performance", "CA1822:Mark members as static")]
+		public bool IsOneOf(string input, string pattern)
+		{
+			return Regex.IsMatch(input, "^(" + pattern + ")$");
+		}
+
+		/// <summary>
+		/// In a specified input string, replaces all strings that match a specified regular expression with a specified
+		/// replacement string.
+		/// </summary>
+		/// <param name="input">
+		/// The string to search for a match.
+		/// </param>
+		/// <param name="pattern">
+		/// The regular expression pattern to match.
+		/// </param>
+		/// <param name="replacement">
+		/// The replacement string.
+		/// </param>
+		/// <returns>
+		/// A new string that is identical to the input string, except that the replacement string takes the place of each matched
+		/// string. If pattern is not matched in the current instance, the method returns the current instance unchanged.
+		/// </returns>
+		/// <seealso cref="Regex.Replace(string,string,string)"/>
+		[SuppressMessage("Performance", "CA1822:Mark members as static")]
+		public string Replace(string input, string pattern, string replacement)
+		{
+			return Regex.Replace(input, pattern, replacement);
+		}
+	}
+}
